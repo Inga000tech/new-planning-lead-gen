d5c7b95ab4bcc0b5a589546bd09174cd2f0a6764
Inga000tech <inger.balaj@gmail.com>
2026-03-09 00:11:23 +0100

    Update maplanning_v16.py

--- maplanning_v16.py
+++ maplanning_v16.py
@@ -12,7 +12,6 @@ from google.oauth2.service_account import Credentials as SACredentials
 import os, json
 import email_digest
 import pdfplumber
-import streamlit as st
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
